e2a0702d9e33c48d7ea3591547bac06a0b3ffc2d
bruce oberg <bruce@oberg.org>
2018-12-06 21:15:58 -0800

    second day, first part solved

---
+++ 2018/aoc-2-1.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+pathThis = sys.argv[0]
+pathBase, ext = os.path.splitext(pathThis)
+pathInput = pathBase + '.txt'
+
+aStrLine = [strLine.strip() for strLine in open(pathInput, 'rt').xreadlines()]
+
+if False:
+    for strLine in aStrLine:
+        print 'IN> ', line,
+
+cMatch2 = 0
+cMatch3 = 0
+
+for strLine in aStrLine:
+
+    mpChCch = dict([(ch, 0) for ch in 'abcdefghijklmnopqrstuvwxyz'])
+
+    for ch in strLine:
+        mpChCch[ch] += 1
+
+    setCch = set([cCh for ch, cCh in mpChCch.iteritems()])
+
+    if 2 in setCch:
+        cMatch2 += 1
+
+    if 3 in setCch:
+        cMatch3 += 1
+
+print cMatch2 * cMatch3
