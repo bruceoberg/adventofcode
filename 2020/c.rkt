95811eb4a2a3e18fdf6f277f9d33017c6eb8e32f
Bruce Oberg <bruce@oberg.org>
2020-12-17 17:51:59 -0800

    day 3 done

--- 2020/c.rkt
+++ 2020/c.rkt
@@ -5,6 +5,8 @@
 
 (require srfi/1) ; for drop-right
 
+(require racket/trace)
+
 ; ; (require racket/runtime-path)
 ; ; (define-runtime-path pathData "1.txt")
 ; (define g_pathData
@@ -133,15 +135,43 @@
 
 (define g_lStr (file->lines g_pathData))
 
-(define g_laCh (map (lambda (str) (list->vector (string->list str)) g_lStr)
-(define g_aaCh (list->vector g_laCh)) 
-(define g_xMax (length (first g_lStr)))
-(define g_yMax (length g_lStr))
+(define g_laCh (map (lambda (str) (list->vector (string->list str))) g_lStr))
+(define g_aaCh (list->vector g_laCh))
+
+(define g_xMax (vector-length (vector-ref g_aaCh 0)))
+(define g_yMax (vector-length g_aaCh))
+
+(displayln g_xMax)
+(displayln g_yMax)
+
 (define g_dx 3)
 (define g_dy 1)
 
-(let go  ([x g_dx] [y g_dy] [cT 0])
-    (define xW (mod x g_xMax))
-    (cond
-        [(< y g_yMax) ]
-        [else cT])))
+(define (CFromXy xy)
+    (match-define (list dx dy) xy)
+    (let go ([x dx] [y dy] [cT 0])
+        (define xW (modulo x g_xMax))
+        (define yW (modulo y g_yMax))
+        (define ch (vector-ref (vector-ref g_aaCh yW) xW))
+        (define xNext (+ x dx))
+        (define yNext (+ y dy))
+        (cond
+            [(>= y g_yMax) cT]
+            [(equal? ch #\.) (go xNext yNext cT)]
+            [else (go xNext yNext (add1 cT))])))
+
+(define g_lXy
+    (list
+        (list 1 1)
+        (list 3 1)
+        (list 5 1)
+        (list 7 1)
+        (list 1 2)
+    )
+)
+
+(define g_lC (map CFromXy g_lXy))
+
+g_lC
+
+(apply * g_lC)
