65adbc5a40dfb2766a4427224c3589cc80f44109
Bruce Oberg <bruce@oberg.org>
2021-01-02 22:46:27 -0800

    day 10 done

--- 2020/10.rkt
+++ 2020/10.rkt
@@ -30,20 +30,52 @@
     )
 )
 
-;g_lNDiff
-
 (define c1 (length (filter (lambda (n) (equal? n 1)) g_lNDiff)))
 (define c2 (length (filter (lambda (n) (equal? n 2)) g_lNDiff)))
 (define c3 (length (filter (lambda (n) (equal? n 3)) g_lNDiff)))
 
-(displayln "ones:")
-c1
+; (displayln "ones:")
+; c1
 
-(displayln "twos:")
-c2
+; (displayln "twos:")
+; c2
 
-(displayln "threes:")
-c3
+; (displayln "threes:")
+; c3
 
-(displayln "product:")
-(* c1 c3)
+; (displayln "product:")
+; (* c1 c3)
+
+(define g_lC1
+    (let go ([lNDiff g_lNDiff] [c1 0] [lC1 '()])
+        (cond
+            ([empty? lNDiff] (reverse (cons c1 lC1)))
+            (else
+                (define nDiff (first lNDiff))
+                (define lNDiffGo (rest lNDiff))
+                (if (equal? nDiff 1)
+                    (go lNDiffGo (add1 c1) lC1)
+                    (go lNDiffGo 0 (cons c1 lC1))
+                )
+            )
+        )
+    )
+)
+
+g_lNDiff
+g_lC1
+(apply max g_lC1)
+
+(define g_c1_2 (length (filter (lambda (c1) (equal? c1 2)) g_lC1)))
+(define g_c1_3 (length (filter (lambda (c1) (equal? c1 3)) g_lC1)))
+(define g_c1_4 (length (filter (lambda (c1) (equal? c1 4)) g_lC1)))
+g_c1_2
+g_c1_3
+g_c1_4
+
+(define g_p2 (expt 2 g_c1_2))
+(define g_p4 (expt 4 g_c1_3))
+(define g_p7 (expt 7 g_c1_4))
+
+(define g_pow (* g_p2 g_p4 g_p7))
+g_pow
